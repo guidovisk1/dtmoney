0499ff083b68240fefcb9b767a6339ece376077d
Felipe <fehlipecardooso@gmail.com>
2022-10-01 19:47:31 -0300

    componente new transation modal

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,38 @@
 import { Dashboard } from "./Components/Dashboard";
 import { Header } from "./Components/Header";
 import { GlobalStyle } from "./styles/global";
+import Modal from 'react-modal'
+import React from "react";
+import { NewTransactionModal } from "./Components/NewTransactionModal";
+
+Modal.setAppElement("#root")
+
 
 export function App() {
+
+const [modalIsOpen, setIsOpen] = React.useState(false)
+
+    function openModal() {
+        setIsOpen(true)
+    }
+
+    function closeModal() {
+        setIsOpen(false)
+    }
+
+
+{
   return (
     <>
-    <Header/>
-    <Dashboard/>
-    <GlobalStyle/>
+      <Header onOpenNewTransactionModal={openModal}/>
+
+      <Dashboard />
+      <NewTransactionModal
+      isOpen = {modalIsOpen}
+      onRequestClose = {closeModal}/>
+      <GlobalStyle />
     </>
   );
 }
 
+}
--- src/Components/Header/index.tsx
+++ src/Components/Header/index.tsx
@@ -1,17 +1,25 @@
 import { StyledComponent } from "styled-components"
 import logoImg from '../../assets/logo.svg'
 import { Container, Content } from "./styles"
+import React from "react";
+import Modal from 'react-modal'
+
+interface HeaderProps{
+    onOpenNewTransactionModal: () => void
+}
 
 
-export function Header(){
-    return(
+
+export function Header({onOpenNewTransactionModal} : HeaderProps) {
+
+    
+    return (
         <Container>
             <Content>
-              <img src={logoImg} alt="dt money logo" />
-              <button type="button">
-             Nova transação
-            </button>
-           </Content>
+                <img src={logoImg} alt="dt money logo" />
+                <button type="button" onClick={onOpenNewTransactionModal} > Nova transação </button>
+                
+            </Content>
         </Container>
-)
+    )
 }
